6289e0f5022e9e88605a15e42b97fd52100fe285
Ajmal Bahawodin <ajmal.bahawodin@so4it.com>
2017-04-09 11:28:39 +0200

    First Commit

---
+++ src/main/java/se/javatar/AfghanTicketApplication.java
@@ -0,0 +1,12 @@
+package se.javatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfghanTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfghanTicketApplication.class, args);
+	}
+}
