f381253909ad3e8de88541c798c0ea1897889dad
Ajmal Bahawodin <ajmal.bahawodin@so4it.com>
2017-05-08 10:25:41 +0200

    refactoring

--- src/main/java/se/javatar/controller/StartPageController.java
+++ src/main/java/se/javatar/controller/StartPageController.java
@@ -1,4 +1,4 @@
-package se.javatar.maincontroller;
+package se.javatar.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
