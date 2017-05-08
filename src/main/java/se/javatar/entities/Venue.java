090beae3bc42cd65a0169a41aefe86025f92a586
Ajmal Bahawodin <ajmal.bahawodin@so4it.com>
2017-05-08 12:22:42 +0200

    added venue and section entity, but they need to be comppleted

---
+++ src/main/java/se/javatar/entities/Venue.java
@@ -0,0 +1,25 @@
+package se.javatar.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author Ajmal Bahawodin {@literal <mailto:ajmal@javatar.se/>}
+ */
+@SuppressWarnings("serial")
+@Entity
+public class Venue implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Address address;
+}
